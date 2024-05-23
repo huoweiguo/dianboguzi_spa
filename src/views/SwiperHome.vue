4980e15660f259f97da11c95e66a969eec38c028
huoweiguo <huoweiguo123>
2024-05-23 16:38:39 +0800

    swiper vertical scroll

---
+++ src/views/SwiperHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="height: 100vh;">
+    <swiper :slides-per-view="1" :space-between="50" :modules="modules" direction="vertical">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script setup lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Mousewheel, Scrollbar } from 'swiper/modules'
+// Import Swiper styles
+import 'swiper/css';
+
+const modules = [Mousewheel, Scrollbar]
+</script>
+
+<style lang="scss">
+.swiper-wrapper {
+  height: 100vh;
+}
+</style>
