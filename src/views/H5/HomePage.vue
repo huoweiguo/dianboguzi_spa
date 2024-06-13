b71843d1fd2f3e8b7ca9edebfb9801d3df933e9e
csy <shuya@163.com>
2024-06-13 16:23:15 +0800

    fix：背景样式修改

--- src/views/H5/HomePage.vue
+++ src/views/H5/HomePage.vue
@@ -2,19 +2,23 @@
   <div class="page">
     <img class="img" src="@/assets/h5/text2.png" alt="" />
     <div class="btn-link">
-      <a href="https://testflight.apple.com/join/XfCWV7AF" target="_blank"><img src="@/assets/h5/btn-i.png" alt="" /></a>
-      <a href="https://down.ez6.com.cn/app.php/86" target="_blank"><img src="@/assets/h5/btn-a.png" alt="" /></a>
+      <a href="https://testflight.apple.com/join/XfCWV7AF" target="_blank"
+        ><img src="@/assets/h5/btn-i.png" alt=""
+      /></a>
+      <a href="https://down.ez6.com.cn/app.php/86" target="_blank"
+        ><img src="@/assets/h5/btn-a.png" alt=""
+      /></a>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive } from "vue";
 </script>
 
 <style lang="scss" scoped>
 .page {
-  background: url('@/assets/h5/img9.png') no-repeat right bottom;
+  background: url("@/assets/h5/img9.png") no-repeat right 200px;
   height: 100%;
   background-size: 70% auto;
 }
