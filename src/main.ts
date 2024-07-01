28eeef8da772464e0c835a54796061fb3f157e32
srd18056414952 <liuhuan@youzi-tech.cn>
2024-07-01 15:49:24 +0800

    feat:增加样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import router from "./router";
 import store from "./store";
 import "../src/styles/reset.css";
+import "../src/styles/buble.css";
+import "../src/styles/corw.css";
+import "../src/styles/snow.css";
 import App from "./App.vue";
 
 const app = createApp(App);
